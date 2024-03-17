c7802e451e1f26716dbc01035a5e8111f67427ee
crisperi <phoenixedian@gmail.com>
2024-03-17 17:28:25 +0300

    welll will ai take the jobs or help us to new heights

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    function handleClick(){alert("Click Me!");}
+    const button = document.getElementById("button");
+    button.addEventListener("click",handleClick);
+
 }
+
+addingEventListener();
